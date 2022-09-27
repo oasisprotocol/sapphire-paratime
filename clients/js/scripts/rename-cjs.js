ab9450c265259a0f7cb9319388348bb441a59ce2
Nick Hynes <nhynes@oasislabs.com>
2022-09-28 02:17:36 +0800

    clients/js: fix Node ESM importability

---
+++ clients/js/scripts/rename-cjs.js
@@ -0,0 +1,15 @@
+/** Renames `.js` files in the /lib/cjs folder to `.cjs` to appease ESM clients. */
+
+import { promises as fs } from 'node:fs';
+import url from 'node:url';
+
+process.chdir(url.fileURLToPath(import.meta.url + '/../../lib/cjs'));
+for (const filename of await fs.readdir('.')) {
+  if (!/\.js$/.test(filename)) continue;
+  const cjs = await fs.readFile(filename, 'utf-8');
+  await fs.writeFile(
+    filename.replace('.js', '.cjs'),
+    cjs.replaceAll(/require\("(.\/[\w_\-.]+)\.js"\)/g, 'require("$1.cjs")'),
+  );
+  await fs.unlink(filename);
+}
