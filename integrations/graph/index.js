ec94075276fed8069b1781ce30c5c3321cd0843c
Nick Hynes <nhynes@oasislabs.com>
2022-09-29 19:13:24 +0800

    graph: fix package.json

--- integrations/graph/index.js
+++ integrations/graph/index.js
@@ -5,9 +5,8 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-const provider = new ethers.providers.JsonRpcProvider(process.env.RPC);
-const signer = ethers.Wallet.createRandom().connect(provider);
-const client = sapphire.wrap(signer);
+const provider = sapphire.wrap(new ethers.getDefaultProvider(process.env.RPC));
+const nobody = Buffer.alloc(20);
 
 app.post('/', async (req, res) => {
   if (req.body.jsonrpc !== '2.0') {
@@ -18,8 +17,8 @@ app.post('/', async (req, res) => {
   let result;
   let { id, method, params } = req.body;
   if (method === 'eth_call') {
-    delete params[0]['gas'];
-    result = await client.call(params[0], params[1]);
+    const { gas, ...txCall } = params[0];
+    result = await provider.call({ from: nobody, ...txCall }, params[1]);
   } else {
     result = await provider.send(method, params);
   }
