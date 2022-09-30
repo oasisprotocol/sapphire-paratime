64b22694a8c5b970ba9bb0c50207582d9f0886b8
Nick Hynes <nhynes@oasislabs.com>
2022-09-30 15:24:07 +0800

    integrations/hardhat: add it

--- examples/hardhat/scripts/run-vigil.ts
+++ examples/hardhat/scripts/run-vigil.ts
@@ -1,15 +1,9 @@
 // Usage: pnpm hardhat run --network <network> scripts/run-vigil.ts
 
 import { ethers } from 'hardhat';
-import * as sapphire from '@oasisprotocol/sapphire-paratime';
 
 async function main() {
-  const signer0 = (await ethers.getSigners())[0];
-  const chainId = await signer0.getChainId();
-  const signer = chainId in sapphire.NETWORKS ? sapphire.wrap(signer0) : signer0;
-  const provider = sapphire.wrap(signer.provider!); // We'll use this to read secrets as an unauthenticated party.
-
-  const Vigil = await ethers.getContractFactory('Vigil', signer);
+  const Vigil = await ethers.getContractFactory('Vigil');
   const vigil = await Vigil.deploy();
   console.log('Vigil deployed to:', vigil.address);
 
@@ -22,7 +16,7 @@ async function main() {
   await tx.wait();
   try {
     console.log('Checking the secret');
-    await vigil.connect(provider).callStatic.revealSecret(0);
+    await vigil.connect(ethers.provider).callStatic.revealSecret(0);
     console.log('Uh oh. The secret was available!');
     process.exit(1);
   } catch (e: any) {
---
+++ integrations/hardhat/index.js
@@ -0,0 +1,18 @@
+const sapphire = require('@oasisprotocol/sapphire-paratime');
+const { extendEnvironment } = require('hardhat/config');
+
+extendEnvironment((hre) => {
+  const { chainId, url: rpcUrl } = hre.network.config;
+  if (chainId) {
+    if (!sapphire.NETWORKS[chainId]) return;
+  } else if (/sapphire/i.test(rpcUrl)) {
+    console.warn(
+      'The Hardhat config for the network with `url`',
+      rpcUrl,
+      'did not specify `chainId`.',
+      'The RPC URL looks like it may be Sapphire, so `@oasisprotocol/sapphire-hardhat` has been activated.',
+      'You can prevent this from happening by setting a non-Sapphire `chainId`.',
+    );
+  }
+  hre.network.provider = sapphire.wrap(hre.network.provider);
+});
