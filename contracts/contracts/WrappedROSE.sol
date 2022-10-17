d6f6c88401bbebaa4ea55ad5c2bf8890f6620290
Nick Hynes <nhynes@oasislabs.com>
2022-10-17 17:11:25 +0800

    Add WrappedROSE contract (#74)

---
+++ contracts/contracts/WrappedROSE.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract WrappedROSE is ERC20, ERC20Burnable {
+    event  Deposit(address indexed dst, uint wad);
+    event  Withdrawal(address indexed src, uint wad);
+
+    constructor() ERC20("Wrapped ROSE", "wROSE") {}
+
+    function deposit() external payable {
+        _deposit();
+    }
+
+    function withdraw(uint256 amount) external {
+        _burn(msg.sender, amount);
+        payable(msg.sender).transfer(amount);
+        emit Withdrawal(msg.sender, amount);
+    }
+
+    receive() external payable {
+        _deposit();
+    }
+
+    function _deposit() internal {
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+}
