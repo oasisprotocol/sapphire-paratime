8825f187c060cd33f9a7d606ba21d7c7c5ec9407
Jan Berčič <jan@oasisprotocol.org>
2022-10-28 17:41:10 +0200

    contracts: Add wrappers for signature precompiles to Sapphire library

--- contracts/contracts/Sapphire.sol
+++ contracts/contracts/Sapphire.sol
@@ -92,4 +92,144 @@ library Sapphire {
         require(success, "decrypt: failed");
         return plaintext;
     }
+
+    /**
+     * @dev Generate a public/private key pair using the specified method and seed.
+     * @param method The method to use (0 - ed25519, 1 - secp256k1, 2 - sr25519).
+     * @param seed The seed to use for generating the key pair.
+     * @return (publicKey, privateKey) The generated key pair.
+     */
+    function generateKeyPair(uint method, bytes memory seed) internal view returns (bytes memory publicKey, bytes memory privateKey) {
+        assembly {
+            let buf := mload(0x40)
+            let seedLen := mload(seed)
+            mstore(buf, method)
+            mstore(add(buf, 0x20), seedLen)
+            for { let i := 0 } lt(i, seedLen) { i := add(i, 0x20) } {
+                mstore(add(buf, add(0x40, i)), mload(add(seed, add(0x20, i))))
+            }
+            let success := staticcall(gas(), 0x0100000000000000000000000000000000000005, buf, add(0x40, seedLen), buf, 0x40)
+            if iszero(success) {
+                revert(0, 0)
+            }
+
+            publicKey := mload(0x40)
+            returndatacopy(publicKey, 0, 0x20)
+            let publicKeyLen := mload(publicKey)
+            returndatacopy(add(publicKey, 0x20), 0x40, publicKeyLen)
+
+            let rounded := and(add(publicKeyLen, 0x1f), not(0x1f))
+            rounded := add(rounded, 0x20)
+            privateKey := add(publicKey, rounded)
+            returndatacopy(privateKey, 0x20, 0x20)
+            let privateKeyLen := mload(privateKey)
+            rounded := add(rounded, 0x20)
+            returndatacopy(add(privateKey, 0x20), rounded, privateKeyLen)
+
+            let total := and(add(privateKeyLen, 0x1f), not(0x1f))
+            total := add(rounded, total)
+            mstore(0x40, add(publicKey, total))
+        }
+    }
+
+    /**
+     * @dev Sign a message within the provided context using the specified method, and return the signature.
+     * @param method The method to use (0 - ed25519, 1 - secp256k1, 2 - sr25519).
+     * @param privateKey The private key to use for signing.
+     * @param context The context to add to the message before signing.
+     * @param message The message to sign.
+     * @return signature The resulting signature.
+     */
+    function signMessageWithContext(uint method, bytes memory privateKey, bytes memory context, bytes memory message) internal view returns (bytes memory signature) {
+        assembly {
+            let buf := mload(0x40)
+            let privateKeyLen := mload(privateKey)
+            let contextLen := mload(context)
+            let messageLen := mload(message)
+            mstore(buf, method)
+            mstore(add(buf, 0x20), privateKeyLen)
+            mstore(add(buf, 0x40), contextLen)
+            mstore(add(buf, 0x60), messageLen)
+            let offset := add(buf, 0x80)
+            privateKeyLen := add(privateKeyLen, 0x20)
+            for { let i := 0x20 } lt(i, privateKeyLen) { i := add(i, 0x20) } {
+                mstore(offset, mload(add(privateKey, i)))
+                offset := add(offset, 0x20)
+            }
+            contextLen := add(contextLen, 0x20)
+            for { let i := 0x20 } lt(i, contextLen) { i := add(i, 0x20) } {
+                mstore(offset, mload(add(context, i)))
+                offset := add(offset, 0x20)
+            }
+            messageLen := add(messageLen, 0x20)
+            for { let i := 0x20 } lt(i, messageLen) { i := add(i, 0x20) } {
+                mstore(add(offset, i), mload(add(message, i)))
+                offset := add(offset, 0x20)
+            }
+            let success := staticcall(gas(), 0x0100000000000000000000000000000000000006, buf, sub(offset, buf), buf, 0x40)
+            if iszero(success) {
+                revert(0, 0)
+            }
+
+            signature := mload(0x40)
+            let signatureLen := returndatasize()
+            mstore(signature, signatureLen)
+            returndatacopy(add(signature, 0x20), 0, signatureLen)
+            let rounded := and(add(signatureLen, 0x1f), not(0x1f))
+            mstore(0x40, add(signature, add(rounded, 0x20)))
+        }
+    }
+
+    /**
+     * @dev Verify that the context and message correspond to the given signature.
+     * @param method The method to use (0 - ed25519, 1 - secp256k1, 2 - sr25519).
+     * @param publicKey The public key to use to check the signature.
+     * @param signature The signature to check.
+     * @param context The context to add to the message before checking.
+     * @param message The message to check.
+     * @return result The result of the verification; `True` if the verification succeeded.
+     */
+    function verifySignatureWithContext(uint method, bytes memory publicKey, bytes memory signature, bytes memory context, bytes memory message) internal view returns (bool result) {
+        assembly {
+            let buf := mload(0x40)
+            let publicKeyLen := mload(publicKey)
+            let signatureLen := mload(signature)
+            let contextLen := mload(context)
+            let messageLen := mload(message)
+            mstore(buf, method)
+            mstore(add(buf, 0x20), publicKeyLen)
+            mstore(add(buf, 0x40), signatureLen)
+            mstore(add(buf, 0x60), contextLen)
+            mstore(add(buf, 0x80), messageLen)
+            let offset := add(buf, 0xa0)
+            publicKeyLen := add(publicKeyLen, 0x20)
+            for { let i := 0x20 } lt(i, publicKeyLen) { i := add(i, 0x20) } {
+                mstore(offset, mload(add(publicKey, i)))
+                offset := add(offset, 0x20)
+            }
+            signatureLen := add(signatureLen, 0x20)
+            for { let i := 0x20 } lt(i, signatureLen) { i := add(i, 0x20) } {
+                mstore(offset, mload(add(signature, i)))
+                offset := add(offset, 0x20)
+            }
+            contextLen := add(contextLen, 0x20)
+            for { let i := 0x20 } lt(i, contextLen) { i := add(i, 0x20) } {
+                mstore(offset, mload(add(context, i)))
+                offset := add(offset, 0x20)
+            }
+            messageLen := add(messageLen, 0x20)
+            for { let i := 0x20 } lt(i, messageLen) { i := add(i, 0x20) } {
+                mstore(add(offset, i), mload(add(message, i)))
+                offset := add(offset, 0x20)
+            }
+            let success := staticcall(gas(), 0x0100000000000000000000000000000000000007, buf, sub(offset, buf), buf, 0x40)
+            if iszero(success) {
+                revert(0, 0)
+            }
+
+            buf := mload(0x40)
+            returndatacopy(buf, 0, returndatasize())
+            result := mload(buf)
+        }
+    }
 }
