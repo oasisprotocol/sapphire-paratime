ede15282c84fb204f0859d33a216f9fea4119b41
Matevž Jekovec <matevz@oasisprotocol.org>
2023-01-20 11:39:11 +0100

    client/js: Add localnet params

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -22,11 +22,19 @@ const testnetParams = {
   runtimeId:
     '0x000000000000000000000000000000000000000000000000a6d1e3ebf60dff6c',
 };
+const localnetParams = {
+  chainId: 0x5afd,
+  defaultGateway: 'http://localhost:8545/',
+  runtimeId:
+    '0x8000000000000000000000000000000000000000000000000000000000000000',
+};
 export const NETWORKS = {
   mainnet: mainnetParams,
   testnet: testnetParams,
+  localnet: localnetParams,
   [mainnetParams.chainId]: mainnetParams,
   [testnetParams.chainId]: testnetParams,
+  [localnetParams.chainId]: localnetParams,
 };
 
 export const OASIS_CALL_DATA_PUBLIC_KEY = 'oasis_callDataPublicKey';
