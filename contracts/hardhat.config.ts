f31d11b1dd1ce95be878d7bc1554a9e95610ad96
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-06-21 20:46:48 +0100

    Improved tests for signing algorithms - tests specific properties of each alg

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,6 +5,15 @@ import '@typechain/hardhat';
 import '@nomicfoundation/hardhat-chai-matchers';
 import 'hardhat-watcher';
 import 'solidity-coverage';
+import { HardhatNetworkHDAccountsUserConfig } from 'hardhat/types';
+
+const TEST_HDWALLET : HardhatNetworkHDAccountsUserConfig = {
+  mnemonic: 'test test test test test test test test test test test junk',
+  path: "m/44'/60'/0'/0",
+  initialIndex: 0,
+  count: 20,
+  passphrase: '',
+};
 
 task('wrap-rose', 'Wrap some ROSE.')
   .addParam('wroseAddr')
@@ -68,24 +77,12 @@ const config: HardhatUserConfig = {
     'sapphire-dev-ci': {
       url: 'http://sapphire-dev-ci:8545',
       chainId: 0x5afd,
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
+      accounts: TEST_HDWALLET
     },
     'sapphire-dev': {
       url: 'http://localhost:8545',
       chainId: 0x5afd,
-      accounts: {
-        mnemonic: 'test test test test test test test test test test test junk',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-        passphrase: '',
-      },
+      accounts: TEST_HDWALLET
     },
   },
   watcher: {
--- contracts/test/precompiles.ts
+++ contracts/test/precompiles.ts
@@ -1,15 +1,59 @@
-// SPDX-License-Identifier: CC-PDDC
+// SPDX-License-Identifier: Apache-2
 
 import { randomBytes } from 'crypto';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
+import { SigningTests__factory } from "../typechain-types/factories/contracts/tests"
+import { SigningTests } from '../typechain-types/contracts/tests/SigningTests';
+
+async function testSignThenVerify(
+  se: SigningTests,
+  alg:number,
+  keypair:{publicKey:string, secretKey:string},
+  ctx:Buffer,
+  msg:Buffer,
+  ctx_len?:number,
+  msg_len?:number
+)
+{
+  const sig = await se.testSign(alg, keypair.secretKey, ctx, msg);
+
+  expect(await se.testVerify(alg, keypair.publicKey, ctx, msg, sig)).equal(true);
+
+  // If message is changed, signature will not be valid
+  if( msg.length != 0 ) {
+    expect(await se.testVerify(alg, keypair.publicKey, ctx, randomBytes(msg.length), sig)).equal(false);
+  }
+
+  // If context is changed, signature will not be valid
+  if( ctx.length != 0 ) {
+    expect(await se.testVerify(alg, keypair.publicKey, randomBytes(ctx.length), msg, sig)).equal(false);
+  }
+
+  if( ctx_len !== undefined ) {
+    if( ctx_len > 1 ) {
+      expect(se.testVerify(alg, keypair.publicKey, randomBytes(ctx_len-1), msg, sig)).to.be.reverted;
+    }
+    expect(se.testVerify(alg, keypair.publicKey, randomBytes(ctx_len+1), msg, sig)).to.be.reverted;
+  }
+
+  if( msg_len !== undefined ) {
+    if( msg_len > 1 ) {
+      expect(se.testVerify(alg, keypair.publicKey, ctx, randomBytes(msg_len-1), sig)).to.be.reverted;
+    }
+    expect(se.testVerify(alg, keypair.publicKey, ctx, randomBytes(msg_len+1), sig)).to.be.reverted;
+  }
+}
+
+const EMPTY_BUFFER = Buffer.from([]);
+
+const VARYING_SIZED_BUFFERS = [EMPTY_BUFFER, randomBytes(1), randomBytes(16), randomBytes(32), randomBytes(64)];
+
 describe('Precompiles', function () {
   async function deploy() {
-    const SigningTests_Contract = await ethers.getContractFactory(
-      'SigningTests',
-    );
-    const se = await SigningTests_Contract.deploy();
+    const factory = await ethers.getContractFactory("SigningTests") as SigningTests__factory;
+    const se = await factory.deploy();
 
     return { se };
   }
@@ -17,165 +61,44 @@ describe('Precompiles', function () {
   it('Sign & Verify', async function () {
     const { se } = await deploy();
 
-    const empty_bytes = new Uint8Array([]);
-
     // Try Ed25519 Oasis (alg=0)
-    const edo_seed = randomBytes(32);
-    const edo_kp = await se.testKeygen(1, edo_seed);
-    for (const edo_ctx of [
-      empty_bytes,
-      randomBytes(1),
-      randomBytes(16),
-      randomBytes(32),
-      randomBytes(64),
-    ]) {
-      for (const edo_msg of [
-        empty_bytes,
-        randomBytes(1),
-        randomBytes(16),
-        randomBytes(32),
-        randomBytes(64),
-      ]) {
-        const edo_sig = await se.testSign(
-          1,
-          edo_kp.secretKey,
-          edo_ctx,
-          edo_msg,
-        );
-        const edo_vfy = await se.testVerify(
-          1,
-          edo_kp.publicKey,
-          edo_ctx,
-          edo_msg,
-          edo_sig,
-        );
-        expect(edo_vfy).equal(true);
+    // Varying sized message and context
+    const edo_kp = await se.testKeygen(1, randomBytes(32));
+    for (const edo_ctx of VARYING_SIZED_BUFFERS) {
+      for (const edo_msg of VARYING_SIZED_BUFFERS) {
+        await testSignThenVerify(se, 0, edo_kp, edo_ctx, edo_msg);
       }
     }
 
     // Try Ed25519 Pure (alg=1)
-    const ed_seed = randomBytes(32);
-    const ed_kp = await se.testKeygen(1, ed_seed);
-    for (const ed_msg of [
-      empty_bytes,
-      randomBytes(1),
-      randomBytes(16),
-      randomBytes(32),
-      randomBytes(64),
-    ]) {
-      const ed_sig = await se.testSign(
-        1,
-        ed_kp.secretKey,
-        empty_bytes,
-        ed_msg,
-      );
-      const ed_vfy = await se.testVerify(
-        1,
-        ed_kp.publicKey,
-        empty_bytes,
-        ed_msg,
-        ed_sig,
-      );
-      expect(ed_vfy).equal(true);
+    // Variying sized message, no context
+    const ed_kp = await se.testKeygen(1, randomBytes(32));
+    for (const ed_msg of VARYING_SIZED_BUFFERS) {
+      await testSignThenVerify(se, 1, ed_kp, EMPTY_BUFFER, ed_msg, 0);
     }
 
     // Try Ed25519 Prehashed SHA-512 (alg=2)
-    const edp512_seed = randomBytes(32);
-    const edp512_kp = await se.testKeygen(2, edp512_seed);
-    const edp512_ctx = randomBytes(64);
-    const edp512_sig = await se.testSign(
-      2,
-      edp512_kp.secretKey,
-      edp512_ctx,
-      empty_bytes,
-    );
-    const edp512_vfy = await se.testVerify(
-      2,
-      edp512_kp.publicKey,
-      edp512_ctx,
-      empty_bytes,
-      edp512_sig,
-    );
-    expect(edp512_vfy).equal(true);
+    const edp512_kp = await se.testKeygen(2, randomBytes(32));
+    await testSignThenVerify(se, 2, edp512_kp, randomBytes(64), EMPTY_BUFFER, 64, 0);
 
     // Try Secp256k1 Oasis (alg=3)
     // Note: https://github.com/oasisprotocol/oasis-sdk/blob/ca630e7d48986bd102d7aa477a48f8966a3d1d23/runtime-sdk/src/crypto/signature/secp256k1.rs#L51
     // Uses SHA512-256 to digest the context then the message, so no pre-hashing (full message must be available, unless it's explicitly pre-hashed)
-    const k1oasis_seed = randomBytes(32);
-    const k1oasis_kp = await se.testKeygen(3, k1oasis_seed);
-    for (const k1oasis_ctx of [
-      empty_bytes,
-      randomBytes(1),
-      randomBytes(16),
-      randomBytes(32),
-      randomBytes(64),
-    ]) {
-      for (const k1oasis_msg of [
-        empty_bytes,
-        randomBytes(1),
-        randomBytes(16),
-        randomBytes(32),
-        randomBytes(64),
-      ]) {
-        const k1oasis_sig = await se.testSign(
-          3,
-          k1oasis_kp.secretKey,
-          k1oasis_ctx,
-          k1oasis_msg,
-        );
-        const k1oasis_vfy = await se.testVerify(
-          3,
-          k1oasis_kp.publicKey,
-          k1oasis_ctx,
-          k1oasis_msg,
-          k1oasis_sig,
-        );
-        expect(k1oasis_vfy).equal(true);
+    const k1oasis_kp = await se.testKeygen(3, randomBytes(32));
+    for (const k1oasis_ctx of VARYING_SIZED_BUFFERS) {
+      for (const k1oasis_msg of VARYING_SIZED_BUFFERS) {
+        await testSignThenVerify(se, 3, k1oasis_kp, k1oasis_ctx, k1oasis_msg);
       }
     }
 
     // Try Secp256k1 prehashed Keccak256 (alg=4)
-    // TODO: verify it fails with non-empty message
-    // TODO: verify it fails when context != 32 bytes
-    // TODO: verify changing any bit of any parameter invalidates
-    const k256_seed = randomBytes(32);
-    const k256_kp = await se.testKeygen(4, k256_seed);
-    const k256_ctx = randomBytes(32);
-    const k256_sig = await se.testSign(
-      4,
-      k256_kp.secretKey,
-      k256_ctx,
-      empty_bytes,
-    );
-    const k256_vfy = await se.testVerify(
-      4,
-      k256_kp.publicKey,
-      k256_ctx,
-      empty_bytes,
-      k256_sig,
-    );
-    expect(k256_vfy).equal(true);
+    // 32 byte context, empty message
+    const k256_kp = await se.testKeygen(4, randomBytes(32));
+    await testSignThenVerify(se, 4, k256_kp, randomBytes(32), EMPTY_BUFFER, 32, 0);
 
     // Try Secp256k1 prehashed SHA256 (alg=5)
-    // TODO: verify it fails with non-empty message
-    // TODO: verify it fails when context != 32 bytes
-    // TODO: verify changing any bit of any parameter invalidates
-    const sha256_seed = randomBytes(32);
-    const sha256_kp = await se.testKeygen(5, sha256_seed);
-    const sha256_ctx = randomBytes(32);
-    const sha256_sig = await se.testSign(
-      5,
-      sha256_kp.secretKey,
-      sha256_ctx,
-      empty_bytes,
-    );
-    const sha256_vfy = await se.testVerify(
-      5,
-      sha256_kp.publicKey,
-      sha256_ctx,
-      empty_bytes,
-      sha256_sig,
-    );
-    expect(sha256_vfy).equal(true);
+    // 32 byte context, empty message
+    const sha256_kp = await se.testKeygen(5, randomBytes(32));
+    await testSignThenVerify(se, 5, sha256_kp, randomBytes(32), EMPTY_BUFFER, 32, 0);
   });
 });
