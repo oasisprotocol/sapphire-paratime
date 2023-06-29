3bff308efb55b149fcc5965974723a8f7f01e8c9
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-06-29 12:12:07 +0100

    Compatibility fix for new & old versions of truffle (#142)

--- clients/js/src/compat.ts
+++ clients/js/src/compat.ts
@@ -512,9 +512,19 @@ export async function fetchRuntimePublicKey(
     // first opportunistically try `send` from the provider
     try {
       const source = provider as {
-        send: (method: string, params: any[]) => Promise<any>;
+        send: (
+          method: string,
+          params: any[] | ((err: any, ok?: any) => void),
+        ) => Promise<any>;
       };
-      const { key } = await source.send(OASIS_CALL_DATA_PUBLIC_KEY, []);
+      const arg =
+        'engine' in provider
+          ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            function (err: any, ok?: any) {
+              return;
+            }
+          : [];
+      const { key } = await source.send(OASIS_CALL_DATA_PUBLIC_KEY, arg);
       if (key) return arrayify(key);
     } catch {
       // don't do anything, move on to try chainId
