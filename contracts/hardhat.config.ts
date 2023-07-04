f2f66410bd31190cbe1abcc35018fe3dc1818396
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-07-04 10:31:57 +0100

    One last attempt at getting sapphire-dev-ci container working

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -75,7 +75,7 @@ const config: HardhatUserConfig = {
         : [],
     },
     sapphire_dev_ci: {
-      url: `http://${process.env.SAPPHIRE_DEV_CI_HTTP_HOST}:${process.env.SAPPHIRE_DEV_CI_HTTP_PORT}`,
+      url: `http://sapphire_dev_ci:8545`,
       chainId: 0x5afd,
       accounts: TEST_HDWALLET,
     },
