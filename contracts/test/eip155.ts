f0f0eaabc79145126f58ec566a5b7f1c1baa589e
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-07-17 17:56:06 +0200

    clients: Nick suggestoins

--- contracts/test/eip155.ts
+++ contracts/test/eip155.ts
@@ -3,36 +3,35 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { EIP155Tests__factory } from '../typechain-types/factories/contracts/tests';
-import { StaticJsonRpcProvider } from '@ethersproject/providers';
 
 describe('EIP-155', function () {
   async function deploy() {
     const factory = (await ethers.getContractFactory(
       'EIP155Tests',
     )) as EIP155Tests__factory;
-    const x = await factory.deploy({ value: ethers.utils.parseEther('1') });
-    return { x };
+    const testContract = await factory.deploy({ value: ethers.utils.parseEther('1') });
+    return { testContract };
   }
 
   it('Signed transactions can be submitted', async function () {
-    const { x } = await deploy();
+    const { testContract } = await deploy();
     const txobj = {
       nonce: 0,
-      gasPrice: await x.provider.getGasPrice(),
+      gasPrice: await testContract.provider.getGasPrice(),
       gasLimit: 250000,
-      to: x.address,
+      to: testContract.address,
       value: 0,
       data: '0x',
       chainId: 0,
     };
-    const signedTx = await x.sign(txobj);
+    const signedTx = await testContract.sign(txobj);
 
     // Submit signed transaction via plain JSON-RPC provider (avoiding saphire.wrap)
-    const plain_provider = new StaticJsonRpcProvider(
+    const plainProvider = new ethers.providers.StaticJsonRpcProvider(
       ethers.provider.connection,
     );
-    let plain_resp = await plain_provider.sendTransaction(signedTx);
-    let receipt = await x.provider.waitForTransaction(plain_resp.hash);
+    let plainResp = await plainProvider.sendTransaction(signedTx);
+    let receipt = await testContract.provider.waitForTransaction(plainResp.hash);
     expect(receipt.logs[0].data).equal(
       '0xfedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210',
     );
