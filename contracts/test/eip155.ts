937967ae4d0df427a750076cab026bef57ab8390
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-09-06 09:07:18 +0100

    contracts: submit pre-signed transaction

--- contracts/test/eip155.ts
+++ contracts/test/eip155.ts
@@ -3,20 +3,38 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { EIP155Tests__factory } from '../typechain-types/factories/contracts/tests';
+import { EIP155Tests } from '../typechain-types/contracts/tests/EIP155Tests';
 
 describe('EIP-155', function () {
-  async function deploy() {
+  let testContract : EIP155Tests;
+  before(async () => {
     const factory = (await ethers.getContractFactory(
       'EIP155Tests',
     )) as EIP155Tests__factory;
-    const testContract = await factory.deploy({
+    testContract = await factory.deploy({
       value: ethers.utils.parseEther('1'),
     });
-    return { testContract };
-  }
+  });
+
+  it('Encrypts pre-signed transactions', async function () {
+    const txobj = {
+      nonce: 0,
+      gasPrice: await testContract.provider.getGasPrice(),
+      gasLimit: 250000,
+      to: testContract.address,
+      value: 0,
+      data: '0x',
+      chainId: 0,
+    };
+    const signedTx = await testContract.sign(txobj);
+    const response = await testContract.provider.sendTransaction(signedTx);
+    const receipt = await response.wait();
+    expect(receipt.logs[0].data).equal(
+      '0xfedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210',
+    );
+  });
 
   it('Signed transactions can be submitted', async function () {
-    const { testContract } = await deploy();
     const txobj = {
       nonce: 0,
       gasPrice: await testContract.provider.getGasPrice(),
