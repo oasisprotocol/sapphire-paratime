781392da22bab1453c0c0d9eeea9a762bc28c3ff
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-09-17 14:54:16 +0200

    integrations/hardhat: remove oasislabs/hardhat fork (#182)

--- integrations/hardhat/src/index.ts
+++ integrations/hardhat/src/index.ts
@@ -1,7 +1,5 @@
 import * as sapphire from '@oasisprotocol/sapphire-paratime';
-import { extendEnvironment, subtask } from 'hardhat/config';
-import { TASK_NODE_GET_PROVIDER } from 'hardhat/builtin-tasks/task-names';
-import { EthereumProvider } from 'hardhat/types';
+import { extendEnvironment } from 'hardhat/config';
 
 import './type-extensions';
 
@@ -23,57 +21,3 @@ extendEnvironment((hre) => {
   }
   hre.network.provider = sapphire.wrap(hre.network.provider);
 });
-
-subtask(TASK_NODE_GET_PROVIDER).setAction(
-  async (
-    args: {
-      forkBlockNumber?: number;
-      forkUrl?: string;
-    },
-    { artifacts, config, network, userConfig },
-    runSuper,
-  ): Promise<EthereumProvider> => {
-    const HARDHAT_NETWORK_NAME = 'hardhat';
-    const { forkBlockNumber: forkBlockNumberParam, forkUrl: forkUrlParam } =
-      args;
-    if (
-      network.name !== HARDHAT_NETWORK_NAME ||
-      (network.config as any).confidential !== true
-    ) {
-      return runSuper();
-    }
-
-    const hardhatNetworkConfig = config.networks[HARDHAT_NETWORK_NAME];
-    const { createProvider } = await import(
-      '@oasislabs/hardhat/internal/core/providers/construction'
-    );
-    const provider = await createProvider(
-      config,
-      HARDHAT_NETWORK_NAME,
-      artifacts,
-    );
-
-    const forkUrlConfig = hardhatNetworkConfig.forking?.url;
-    const forkBlockNumberConfig = hardhatNetworkConfig.forking?.blockNumber;
-
-    const forkUrl = forkUrlParam ?? forkUrlConfig;
-    const forkBlockNumber = forkBlockNumberParam ?? forkBlockNumberConfig;
-
-    if (forkBlockNumber || forkUrl) {
-      throw new Error(
-        'Sapphire forking is not yet supported. Please file an issue if you see this error!',
-      );
-    }
-
-    const hardhatNetworkUserConfig =
-      userConfig.networks?.[HARDHAT_NETWORK_NAME] ?? {};
-
-    // enable logging
-    await provider.request({
-      method: 'hardhat_setLoggingEnabled',
-      params: [hardhatNetworkUserConfig.loggingEnabled ?? true],
-    });
-
-    return provider;
-  },
-);
