88cf4af2e6876ebd9473224de555ecc004791d06
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-09-18 14:36:05 +0100

    contracts: eip-155: removed dependence on hard-coded secret

--- contracts/test/eip155.ts
+++ contracts/test/eip155.ts
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import hre, { ethers } from 'hardhat';
 import * as sapphire from '@oasisprotocol/sapphire-paratime';
 import { EIP155Tests__factory } from '../typechain-types/factories/contracts/tests';
 import { EIP155Tests } from '../typechain-types/contracts/tests/EIP155Tests';
+import { HardhatNetworkHDAccountsConfig } from 'hardhat/types';
 
 const EXPECTED_EVENT =
   '0xfedcba9876543210fedcba9876543210fedcba9876543210fedcba9876543210';
@@ -21,8 +22,18 @@ function entropy(str: string) {
     }, 0);
 }
 
+function getWallet(index: number) {
+  const accounts = hre.network.config
+    .accounts as HardhatNetworkHDAccountsConfig;
+  return ethers.Wallet.fromMnemonic(
+    accounts.mnemonic,
+    accounts.path + `/${index}`,
+  );
+}
+
 describe('EIP-155', function () {
   let testContract: EIP155Tests;
+  let calldata: string;
   before(async () => {
     const factory = (await ethers.getContractFactory(
       'EIP155Tests',
@@ -31,14 +42,13 @@ describe('EIP-155', function () {
       value: ethers.utils.parseEther('1'),
     });
     await testContract.deployed();
+    calldata = testContract.interface.encodeFunctionData('example');
   });
 
   it('Wrapper encrypts self-signed transaction calldata', async function () {
     const tx = await testContract.example();
     expect(entropy(tx.data)).gte(3.8);
-    expect(tx.data).not.eq(
-      testContract.interface.encodeFunctionData('example'),
-    );
+    expect(tx.data).not.eq(calldata);
     expect(tx.data.length).eq(218);
   });
 
@@ -64,18 +74,17 @@ describe('EIP-155', function () {
     let receipt = await testContract.provider.waitForTransaction(
       plainResp.hash,
     );
-    expect(plainResp.data).eq(
-      testContract.interface.encodeFunctionData('example'),
-    );
+    expect(plainResp.data).eq(calldata);
     expect(receipt.logs[0].data).equal(EXPECTED_EVENT);
   });
 
   it('Other-Signed transaction submission via wrapped provider', async function () {
+    const provider = testContract.provider;
     const signedTx = await testContract.sign({
-      nonce: await testContract.provider.getTransactionCount(
+      nonce: await provider.getTransactionCount(
         await testContract.publicAddr(),
       ),
-      gasPrice: await testContract.provider.getGasPrice(),
+      gasPrice: await provider.getGasPrice(),
       gasLimit: 250000,
       to: testContract.address,
       value: 0,
@@ -83,22 +92,15 @@ describe('EIP-155', function () {
       chainId: 0,
     });
 
-    let plainResp = await testContract.provider.sendTransaction(signedTx);
-    let receipt = await testContract.provider.waitForTransaction(
-      plainResp.hash,
-    );
-    expect(plainResp.data).eq(
-      testContract.interface.encodeFunctionData('example'),
-    );
+    let plainResp = await provider.sendTransaction(signedTx);
+    let receipt = await provider.waitForTransaction(plainResp.hash);
+    expect(plainResp.data).eq(calldata);
     expect(receipt.logs[0].data).equal(EXPECTED_EVENT);
   });
 
-  it('Self-Signed transaction submission via wrapped provider', async function () {
+  it('Self-Signed transaction submission via wrapped wallet', async function () {
     const provider = testContract.provider;
-    const sk =
-      '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-    const wallet = sapphire.wrap(new ethers.Wallet(sk).connect(provider));
-    const calldata = testContract.interface.encodeFunctionData('example');
+    const wallet = sapphire.wrap(getWallet(0).connect(provider));
 
     const signedTx = await wallet.signTransaction({
       gasLimit: 250000,
@@ -110,10 +112,14 @@ describe('EIP-155', function () {
       nonce: await provider.getTransactionCount(wallet.address),
     });
 
+    // Calldata should be encrypted when we wrap the wallet provider
     let x = await provider.sendTransaction(signedTx);
+    expect(entropy(x.data)).gte(3.8);
     expect(x.data).not.eq(calldata);
 
     let r = await provider.waitForTransaction(x.hash);
     expect(r.logs[0].data).equal(EXPECTED_EVENT);
   });
+
+  // TODO: test error conditions?
 });
