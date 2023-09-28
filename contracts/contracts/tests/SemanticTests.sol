5778a8b64cdb92696e25b4d96f0ff58bc7dcb9eb
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-09-28 20:15:24 +0100

    contracts: added tests for custom errors

--- contracts/contracts/tests/SemanticTests.sol
+++ contracts/contracts/tests/SemanticTests.sol
@@ -7,4 +7,18 @@ contract SemanticTests {
     {
         return new bytes(len);
     }
+
+    error CustomError(uint value);
+    uint private x;
+
+    uint constant ERROR_NUM = 0x1023456789abcdef1023456789abcdef1023456789abcdef1023456789abcdef;
+
+    function testCustomRevert () external {
+        x += 1;
+        revert CustomError(ERROR_NUM);
+    }
+
+    function testCustomViewRevert () external pure returns (uint) {
+        revert CustomError(ERROR_NUM);
+    }
 }
--- contracts/test/semantics.ts
+++ contracts/test/semantics.ts
@@ -5,6 +5,8 @@ import { expect } from 'chai';
 import { SemanticTests } from '../typechain-types/contracts/tests/SemanticTests';
 import { SemanticTests__factory } from '../typechain-types/factories/contracts/tests';
 
+const ERROR_NUM = '0x1023456789abcdef1023456789abcdef1023456789abcdef1023456789abcdef';
+
 describe('EVM Semantics', () => {
     let c: SemanticTests;
     before(async () => {
@@ -12,11 +14,42 @@ describe('EVM Semantics', () => {
         c = await f.deploy();
         await c.deployed();
     });
+
     it('eth_call maximum return length vs gas limit', async () => {
         const i = 1787872;
         const respHex = await c.testViewLength(i);
         const respBytes = ethers.utils.arrayify(respHex);
         expect(respBytes.length).eq(i);
         expect(c.testViewLength(i+1)).reverted;
-    })
+    });
+
+    // Apparently `expect().to.be.revertedWithCustomError` doesn't work sometimes
+    // Otherwise we could do this:
+    //expect(c.testCustomRevert()).to.be.revertedWithCustomError(c, 'CustomError').withArgs(errorNum);
+    //expect(c.testCustomViewRevert()).to.be.revertedWithCustomError(c, 'CustomError').withArgs(errorNum);
+
+    it('Custom revert in view call', async () => {
+        // Perform view call, which is expected to revert
+        try {
+            await c.testCustomViewRevert();
+            expect(false).to.be.true;
+        }
+        catch( x: any ) {
+            expect(x.errorArgs[0]).to.eq(ERROR_NUM);
+            expect(x.errorName).to.eq('CustomError');
+        }
+    });
+
+    it('Custom error in tx', async () => {
+        // Perform transaction which is expected to revert
+        try {
+            const tx = await c.testCustomRevert();
+            console.log('txhash', tx.hash);
+            await tx.wait();
+            expect(false).to.be.true;
+        }
+        catch( x: any ) {
+            expect(x.data).eq('0x110b3655' + ERROR_NUM.slice(2));
+        }
+    });
 });
