8f059f294ac0366ed6cd3be86f3e51c3926460d7
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-01-25 23:12:57 +0000

    contracts: formatting

--- contracts/contracts/tests/Omnibus.sol
+++ contracts/contracts/tests/Omnibus.sol
@@ -8,9 +8,7 @@ import {SemanticTests} from "./SemanticTests.sol";
 contract Omnibus is SignedQueriesTests, SemanticTests {
     uint256 public somevar;
 
-    function setSomevar(uint256 value)
-        external
-    {
+    function setSomevar(uint256 value) external {
         somevar = value;
     }
 }
