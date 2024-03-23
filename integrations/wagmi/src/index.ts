881c8f6a7615019b50a4a989f890dcc3ccc96945
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-03-23 16:35:08 +0000

    wagmi: added docs & minor cleanup

--- integrations/wagmi/src/index.ts
+++ integrations/wagmi/src/index.ts
@@ -2,12 +2,32 @@ import { wrapEIP1193Provider, wrap } from '@oasisprotocol/sapphire-paratime';
 import type { Transport } from '@wagmi/core';
 import { injected, custom } from '@wagmi/core';
 
-const cachedProviders: Record<any, any> = {};
-
 type Window = {
   ethereum?: any;
 };
 
+const cachedProviders: Record<any, any> = {};
+
+/**
+ * Wrap the `window.ethereum` with the Sapphire encryption layer.
+ * Used to provide encrypted transactions and calldata to Wagmi browser dApps.
+ *
+ * Example:
+ * ```
+ *
+ *    import { injectedWithSapphire } from '@oasisprotocol/sapphire-wagmi';
+ *
+ *    export const config = createConfig({
+ *      connectors: [
+ *        injectedWithSapphire()
+ *      ],
+ *      ...
+ *    });
+ *
+ * ```
+ *
+ * @returns Same as injected()
+ */
 export function injectedWithSapphire(): ReturnType<typeof injected> {
   return injected({
     target: () => {
@@ -17,8 +37,8 @@ export function injectedWithSapphire(): ReturnType<typeof injected> {
         provider(window?: Window) {
           if (window?.ethereum) {
             // Note: providers are cached as connectors are frequently retrieved
-            //       this prevents the sapphire wrap function being called multiple
-            //       times and spamming RPC with oasis_callDataPublicKey requests
+            //       it prevents sapphire wrapper being called multiple times
+            //       which spams the RPC with oasis_callDataPublicKey requests
             if (!(window.ethereum in cachedProviders)) {
               cachedProviders[window.ethereum] = wrapEIP1193Provider(
                 window.ethereum,
@@ -33,6 +53,25 @@ export function injectedWithSapphire(): ReturnType<typeof injected> {
   });
 }
 
+/**
+ * Provide a Sapphire encrypted RPC transport for Wagmi or Viem.
+ *
+ * Example:
+ * ```
+ *
+ *    import { sapphireTransport } from '@oasisprotocol/sapphire-wagmi';
+ *
+ *    export const config = createConfig({
+ *      transports: {
+ *        [sapphireTestnet.id]: sapphireTransport()
+ *      },
+ *      ...
+ *    });
+ *
+ * ```
+ *
+ * @returns Same as custom()
+ */
 export function sapphireTransport(): Transport {
   return (params) => {
     const p = wrap(params.chain!.rpcUrls.default.http[0]);
