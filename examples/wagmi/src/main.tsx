01f81ddc7671e2c01f3ab103ca1b6b63ea4832ac
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-03-29 11:15:32 +0000

    examples: formatting of wagmi example

--- examples/wagmi/src/main.tsx
+++ examples/wagmi/src/main.tsx
@@ -1,24 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-//import { Buffer } from 'buffer'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { WagmiProvider } from "wagmi";
 
-import App from './App.tsx'
-import { config } from './wagmi.ts'
+import App from "./App.tsx";
+import { config } from "./wagmi.ts";
 
-import './index.css'
+import "./index.css";
 
-//globalThis.Buffer = Buffer
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<App />
+			</QueryClientProvider>
+		</WagmiProvider>
+	</React.StrictMode>,
+);
--- examples/wagmi/src/wagmi.ts
+++ examples/wagmi/src/wagmi.ts
@@ -1,39 +1,38 @@
-import { defineChain } from 'viem';
-import { createConfig } from 'wagmi';
-import { sapphireTestnet } from 'wagmi/chains';
-import { injectedWithSapphire, sapphireTransport } from '@oasisprotocol/sapphire-wagmi';
+import { defineChain } from "viem";
+import { createConfig } from "wagmi";
+import { sapphire, sapphireTestnet } from "wagmi/chains";
+import {
+	injectedWithSapphire,
+	sapphireTransport,
+} from "@oasisprotocol/sapphire-wagmi";
 
 const sapphireLocalnet = defineChain({
-  id: 0x5afd,
-  name: 'Oasis Sapphire Localnet',
-  network: 'sapphire-localnet',
-  nativeCurrency: { name: 'Sapphire Local Rose', symbol: 'TEST', decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ['http://localhost:8545'],
-      webSocket: ['ws://localhost:8546/ws'],
-    },
-  },
-  testnet: true,
+	id: 0x5afd,
+	name: "Oasis Sapphire Localnet",
+	network: "sapphire-localnet",
+	nativeCurrency: { name: "Sapphire Local Rose", symbol: "TEST", decimals: 18 },
+	rpcUrls: {
+		default: {
+			http: ["http://localhost:8545"],
+			webSocket: ["ws://localhost:8546/ws"],
+		},
+	},
+	testnet: true,
 });
 
 export const config = createConfig({
-  multiInjectedProviderDiscovery: false,
-  chains: [
-    sapphireTestnet,
-    sapphireLocalnet
-  ],
-  connectors: [
-    injectedWithSapphire()
-  ],
-  transports: {
-    [sapphireTestnet.id]: sapphireTransport(),
-    [sapphireLocalnet.id]: sapphireTransport()
-  },
-})
+	multiInjectedProviderDiscovery: false,
+	chains: [sapphire, sapphireTestnet, sapphireLocalnet],
+	connectors: [injectedWithSapphire()],
+	transports: {
+		[sapphire.id]: sapphireTransport(),
+		[sapphireTestnet.id]: sapphireTransport(),
+		[sapphireLocalnet.id]: sapphireTransport(),
+	},
+});
 
-declare module 'wagmi' {
-  interface Register {
-    config: typeof config
-  }
+declare module "wagmi" {
+	interface Register {
+		config: typeof config;
+	}
 }
