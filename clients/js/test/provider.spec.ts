7bfd26377d000058087c1dba4e97c660bc11f7fc
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-04-24 13:40:06 +0100

    formatting

--- clients/js/test/provider.spec.ts
+++ clients/js/test/provider.spec.ts
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-  Wallet,
-  JsonRpcProvider,
-  BaseContract,
-  BrowserProvider,
-} from 'ethers';
+import { Wallet, JsonRpcProvider, BaseContract, BrowserProvider } from 'ethers';
 import {
   wrapEthereumProvider,
   NETWORKS,
