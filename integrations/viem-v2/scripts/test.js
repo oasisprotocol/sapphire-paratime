a2d117e3fd042652873710da792a50ef48ea287e
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-09-10 16:20:42 +0100

    integrations/viem: add test to verify Node doesn't hang

---
+++ integrations/viem-v2/scripts/test.js
@@ -0,0 +1,28 @@
+/*
+This script verifies that when Viem is used using Node via the CLI it won't
+prevent Node from exiting cleanly.
+
+See: https://github.com/oasisprotocol/sapphire-paratime/pull/383
+*/
+
+import { createPublicClient, defineChain } from "viem";
+import { sapphireHttpTransport, sapphireLocalnet, createSapphireSerializer } from '@oasisprotocol/sapphire-viem-v2';
+
+const transport = sapphireHttpTransport();
+const chain = sapphireLocalnet;
+const publicClient = createPublicClient({ chain, transport });
+defineChain({
+    id: 0x5afd,
+    name: "Oasis Sapphire Localnet",
+    network: "sapphire-localnet",
+    nativeCurrency: { name: "Sapphire Local Rose", symbol: "TEST", decimals: 18 },
+    rpcUrls: {
+        default: {
+            http: ["http://localhost:8545"],
+        },
+    },
+    serializers: {
+        transaction: await createSapphireSerializer(publicClient)
+    },
+    testnet: true,
+});
