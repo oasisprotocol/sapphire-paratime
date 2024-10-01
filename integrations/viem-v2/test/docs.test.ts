9c56c7bbecbe622691a6af7f2f2ebf1a3ac848bb
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-10-01 05:02:27 +0100

    integrations/viem: make lint happy

--- integrations/viem-v2/test/docs.test.ts
+++ integrations/viem-v2/test/docs.test.ts
@@ -5,12 +5,13 @@ import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 const EXTRACT_CODE_RX = /\`\`\`(?<lang>\w+)\s*(?<content>[\s\S]*?)\`\`\`/gm;
 
-function extractCode (markdown:string) {
+function extractCode(markdown: string) {
 	return Array.from(markdown.matchAll(EXTRACT_CODE_RX), (_) => {
 		return {
-			lang:_.groups?.['lang'],
-			content:_.groups!['content']
-		}});
+			lang: _.groups?.lang,
+			content: _.groups?.content,
+		};
+	});
 }
 
 const TEST_DIR = join(process.cwd(), "test-modules");
@@ -26,11 +27,13 @@ describe("README.md", async () => {
 		await readFile("./README.md", "utf8"),
 	).entries()) {
 		const content = c.content;
-		it(`Code snippet #${index + 1}`, async () => {
-			const fileName = join(TEST_DIR, `example_${index + 1}.ts`);
-			await writeFile(fileName, content);
-			console.log(fileName, content);
-			await expect(import(fileName)).resolves.not.toThrow();
-		});
+		if (content) {
+			it(`Code snippet #${index + 1}`, async () => {
+				const fileName = join(TEST_DIR, `example_${index + 1}.ts`);
+				await writeFile(fileName, content);
+				console.log(fileName, content);
+				await expect(import(fileName)).resolves.not.toThrow();
+			});
+		}
 	}
 });
