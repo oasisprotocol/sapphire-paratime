240b2f6be340f982308e97b65862f78788a2e237
CedarMist <134699267+CedarMist@users.noreply.github.com>
2024-10-01 04:59:03 +0100

    integrations/viem: remove @zaeny/literate

--- integrations/viem-v2/test/docs.test.ts
+++ integrations/viem-v2/test/docs.test.ts
@@ -1,8 +1,17 @@
 import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
-import { extractCode } from "@zaeny/literate";
 import { rimraf } from "rimraf";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+const EXTRACT_CODE_RX = /\`\`\`(?<lang>\w+)\s*(?<content>[\s\S]*?)\`\`\`/gm;
+
+function extractCode (markdown:string) {
+	return Array.from(markdown.matchAll(EXTRACT_CODE_RX), (_) => {
+		return {
+			lang:_.groups?.['lang'],
+			content:_.groups!['content']
+		}});
+}
 
 const TEST_DIR = join(process.cwd(), "test-modules");
 
@@ -20,6 +29,7 @@ describe("README.md", async () => {
 		it(`Code snippet #${index + 1}`, async () => {
 			const fileName = join(TEST_DIR, `example_${index + 1}.ts`);
 			await writeFile(fileName, content);
+			console.log(fileName, content);
 			await expect(import(fileName)).resolves.not.toThrow();
 		});
 	}
