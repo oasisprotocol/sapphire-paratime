8c50bdb19ca85e42af39c916a6c89135bd79a995
Matej Lubej <matej.lubej@oasisprotocol.org>
2025-03-26 20:38:40 +0100

    sapphire-wagmi-v2: Add EIP-6963 support

---
+++ integrations/wagmi-v2/src/constants.ts
@@ -0,0 +1,12 @@
+/**
+ * @license Apache-2.0
+ */
+
+import type { Rdns } from "./types.js";
+
+export const SUPPORTED_RDNS: Rdns[] = [
+	"io.metamask",
+	"io.metamask.mobile",
+	"com.brave.wallet",
+];
+export const EIP6963_ANNOUNCE_PROVIDER_EVENT_NAME = "eip6963:announceProvider";
