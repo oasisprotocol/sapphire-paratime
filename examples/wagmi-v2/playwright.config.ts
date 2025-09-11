3fd4b56275985cfdd215b558f55cc32455d6315f
lubej <matej.lubej@oasisprotocol.org>
2025-09-11 13:35:58 +0200

    wagmi-v2-example: Increase timeouts in CI

--- examples/wagmi-v2/playwright.config.ts
+++ examples/wagmi-v2/playwright.config.ts
@@ -4,15 +4,15 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 2 * 60 * 1000,
+  timeout: process.env.CI ? 5 * 60 * 1000 : 2 * 60 * 1000, // 5 minutes in CI, 2 minutes locally
   testDir: "./test/",
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 2 : 0, // 2 retries in CI, 0 locally
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 1 : undefined, // Use 1 worker in CI
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html"], ["list", { printSteps: true }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -42,5 +42,6 @@ export default defineConfig({
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
     stderr: 'pipe',
+    timeout: process.env.CI ? 120 * 1000 : 60 * 1000, // 2 minutes in CI, 1 minute locally
   },
 });
