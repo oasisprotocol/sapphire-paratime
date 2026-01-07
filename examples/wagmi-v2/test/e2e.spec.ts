721ab78f465a019c6b73489e533b8d6ecf83093d
lubej <matej.lubej@oasisprotocol.org>
2026-01-07 13:20:48 +0100

    wagmi-v2-example: Update config

--- examples/wagmi-v2/test/e2e.spec.ts
+++ examples/wagmi-v2/test/e2e.spec.ts
@@ -1,45 +1,51 @@
-import { BrowserContext, expect, test as baseTest } from "@playwright/test";
+import { BrowserContext, expect, test as base } from "@playwright/test";
 import dappwright, { Dappwright, MetaMaskWallet } from "@tenkeylabs/dappwright";
 
-baseTest.describe.configure({ mode: "serial" });
+base.describe.configure({ mode: "serial" });
 
-export const test = baseTest.extend<{
-	context: BrowserContext;
-	wallet: Dappwright;
-}>({
-	context: async ({}, use) => {
-		// Launch context with extension
-		const [wallet, _, context] = await dappwright.bootstrap("", {
-			wallet: "metamask",
-			version: MetaMaskWallet.recommendedVersion,
-			seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
-			headless: !!process.env.CI,
-		});
+export const test = base.extend<
+	{ wallet: Dappwright },
+	{ walletContext: BrowserContext }
+>({
+	walletContext: [
+		async ({}, use) => {
+			// Launch context with extension
+			const [wallet, _, context] = await dappwright.bootstrap("", {
+				wallet: "metamask",
+				version: MetaMaskWallet.recommendedVersion,
+				seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
+				headless: false,
+			});
 
-		// Add Sapphire Localnet as a custom network
-		await wallet.addNetwork({
-			networkName: "Sapphire Localnet",
-			rpc: "http://localhost:8545",
-			chainId: 23293,
-			symbol: "ROSE",
-		});
+			// Add Sapphire Localnet as a custom network
+			await wallet.addNetwork({
+				networkName: "Sapphire Localnet",
+				rpc: "http://localhost:8545",
+				chainId: 23293,
+				symbol: "ROSE",
+			});
 
-		// Add Anvil Localnet as a custom network
-		await wallet.addNetwork({
-			networkName: "Anvil Localnet",
-			rpc: "http://localhost:9545",
-			chainId: 31337,
-			symbol: "ETH",
-		});
+			// Add Anvil Localnet as a custom network
+			await wallet.addNetwork({
+				networkName: "Anvil Localnet",
+				rpc: "http://localhost:9545",
+				chainId: 31337,
+				symbol: "ETH",
+			});
 
-		await wallet.switchNetwork("Sapphire Localnet");
+			await wallet.switchNetwork("Sapphire Localnet");
 
-		await use(context);
+			await use(context);
+			await context.close();
+		},
+		{ scope: "worker" },
+	],
+	context: async ({ walletContext }, use) => {
+		await use(walletContext);
 	},
-
-	wallet: async ({ context }, use) => {
-		const metamask = await dappwright.getWallet("metamask", context);
-		await use(metamask);
+	wallet: async ({ walletContext }, use) => {
+		const wallet = await dappwright.getWallet("metamask", walletContext);
+		await use(wallet);
 	},
 });
 
